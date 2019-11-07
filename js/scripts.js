af7801becfe23b0d66dc0c354618057e73a1b16f
Eric Halliday and Jaki Widby <eric+jaki@>
2019-11-06 20:41:05 -0800

    create farenheit to celsius calculator

--- js/scripts.js
+++ js/scripts.js
@@ -37,3 +37,17 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
 alert(divide(number1, number2));
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var farenheit = parseInt(prompt("What is the temperature in Farenheit?"));
+
+var celsius = subtract(farenheit, 32);
+
+alert(divide(celsius,1.8))
