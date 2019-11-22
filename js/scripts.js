4474fbe2f1ff73085116a76dbf09ade87cc042e8
em221 <emhd221@gmail.com>
2019-11-21 22:45:55 -0800

    create a basic webpage about voting with an img that clicks to display links.

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,11 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
+$(document).ready(function(){
+  $("button.18ormore").click(function(){
+    $(".over18").show();
+    $(".under18").hide();
+  });
 
-var number1 = parseInt(prompt("Enter a number:"));
-
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(subtract(number1, number2));
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(multiply(number1, number2));
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(divide(number1, number2));
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-var farenheit = parseInt(prompt("What is the temperature in Farenheit?"));
-
-var celsius = subtract(farenheit, 32);
-
-alert(divide(celsius,1.8))
+  $("button.17orless").click(function(){
+    $(".under18").show();
+    $(".over18").hide();
+  });
+});
